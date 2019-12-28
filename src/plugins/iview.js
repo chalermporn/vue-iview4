2424875e494ce7ee345d8432a546ad38cef7959b
Bird <bird@BIRD-MBP.local>
2019-12-28 22:48:12 +0700

    add custom theme iview4

--- src/plugins/iview.js
+++ src/plugins/iview.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import ViewUI from 'view-design'
 import locale from 'view-design/dist/locale/th-TH'
 
-import '../iview-variables.less'
+import '@/styles/index.less'
 
 Vue.use(ViewUI, { locale })
